2e2388172c6347d7e3e847a198a2f13876644191
Michal Budzowski <michal.budzowski@sonarsource.com>
2022-09-16 11:40:43 +0200

    Add project files.

---
+++ ConsoleApp47/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InternalRecursion(50);
+            Pow(3, 2);
+        }
+
+        static int Pow(int num, int exponent)   // Noncompliant; no condition under which pow isn't re-called
+        {
+            num = num * Pow(num, exponent - 1);
+            return num;  // this is never reached
+        }
+
+        static void InternalRecursion(int i)
+        {
+            start:
+            goto end;
+            end:
+            goto start; // Noncompliant; there's no way to break out of this method
+        }
+    }
+
+
+}
